fa93488b29d9242545d1def220c10b73549dc031
yuliadavy <yulia.davyy@gmail.com>
2026-03-06 16:37:36 +0700

    upload2

--- задача 4.py
+++
@@ -1,8 +0,0 @@
-def number_of_unique_characters(data):
-    sym={}
-    for i in data:
-        sym[i]=sym.get(i,0)+1
-    return sym
-data='asdfnkkjbsdff'
-print(number_of_unique_characters(data))
-
